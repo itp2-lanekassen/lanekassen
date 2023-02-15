71ddab57c98dde492f5ee8d4c9478da30f276387
Synne Ødegaard <synne.odegaard@gmail.com>
2023-02-15 11:09:07 +0100

    Configure SPA

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+//import { msalConfig } from "./authConfig";
+const msalConfig = require('./authConfig');
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </StrictMode>
 );
