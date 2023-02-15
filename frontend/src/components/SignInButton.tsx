b638b9250103150074a84a3e823fed5ef0be8884
Synne Ødegaard <synne.odegaard@gmail.com>
2023-02-15 11:41:27 +0100

    Add signin button that redirects to login

---
+++ frontend/src/components/SignInButton.tsx
@@ -0,0 +1,23 @@
+import { useMsal } from '@azure/msal-react';
+import Button from 'react-bootstrap/Button';
+import { loginRequest } from '../authConfig';
+
+/**
+ * Renders a button which, when selected, will redirect the page to the login prompt
+ */
+export const SignInButton = () => {
+  const { instance } = useMsal();
+
+  const handleLogin = (loginType: string) => {
+    if (loginType === 'redirect') {
+      instance.loginRedirect(loginRequest).catch((e) => {
+        console.error;
+      });
+    }
+  };
+  return (
+    <Button variant="secondary" className="ml-auto" onClick={() => handleLogin('redirect')}>
+      Sign in using Redirect
+    </Button>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
-//import { msalConfig } from "./authConfig";
-const msalConfig = require('./authConfig');
+import { msalConfig } from './authConfig';
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
