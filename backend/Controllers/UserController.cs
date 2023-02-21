6620899105da156c5e7bfac222ea1a370dd5cf0e
Martin Isaksen <martin.isaksen@live.no>
2023-02-21 22:41:19 +0100

    #12 Add CRUD functionality

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -186,6 +186,86 @@ namespace Lanekassen.Controllers {
       var users = await _context.Users.ToListAsync();
       return Ok(users);
     }
+
+    //update user
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] UserDTO user) {
+      if (!ModelState.IsValid) {
+        return BadRequest(ModelState);
+      }
+
+      var section = await _context.Sections.FindAsync(user.SectionId);
+      if (section == null) {
+        return BadRequest("Invalid section id");
+      }
+
+      var userToUpdate = await _context.Users.FindAsync(id);
+      if (userToUpdate == null) {
+        return NotFound();
+      }
+
+      userToUpdate.FirstName = user.FirstName;
+      userToUpdate.LastName = user.LastName;
+      userToUpdate.Email = user.Email;
+      userToUpdate.EmploymentType = user.EmploymentType;
+      userToUpdate.Admin = user.Admin;
+      userToUpdate.Section = section;
+      userToUpdate.SubjectFields = await _context.SubjectFields.Where(sf => user.SubjectFields.Contains(sf.SubjectFieldId)).ToListAsync();
+      userToUpdate.Roles = await _context.Roles.Where(r => user.Roles.Contains(r.RoleId)).ToListAsync();
+      userToUpdate.Teams = await _context.Teams.Where(t => user.Teams.Contains(t.TeamId)).ToListAsync();
+      userToUpdate.Absences = await _context.Absences.Where(a => user.Absences.Contains(a.AbsenceId)).ToListAsync();
+
+      try {
+        await _context.SaveChangesAsync();
+      } catch (DbUpdateException ex) {
+        _logger.LogError(ex, "Error updating user");
+        return StatusCode(
+            500,
+            new {
+              message = "Error updating user",
+              error = ex.Message
+            }
+        );
+      }
+
+      return Ok();
+    }
+
+    //delete user
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id) {
+      var userToDelete = await _context.Users.FindAsync(id);
+      if (userToDelete == null) {
+        return NotFound();
+      }
+
+      try {
+        _context.Users.Remove(userToDelete);
+        await _context.SaveChangesAsync();
+      } catch (DbUpdateException ex) {
+        _logger.LogError(ex, "Error deleting user");
+        return StatusCode(
+            500,
+            new {
+              message = "Error deleting user",
+              error = ex.Message
+            }
+        );
+      }
+
+      return Ok();
+    }
+
+    //get user by id
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id) {
+      var user = await _context.Users.FindAsync(id);
+      if (user == null) {
+        return NotFound();
+      }
+
+      return Ok(user);
+    }
   }
 }
 
