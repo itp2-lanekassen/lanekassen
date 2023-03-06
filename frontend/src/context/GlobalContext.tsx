62127fd61f29afb86784facb4420cfd4f801d15d
Simen Sagholen Førrisdal <simenforris@gmail.com>
2023-03-06 11:30:16 +0100

    add section, data etc to global context

--- frontend/src/context/GlobalContext.tsx
+++ frontend/src/context/GlobalContext.tsx
@@ -1,6 +1,11 @@
 import { getAllAbsenceTypes } from '@/API/AbsenceTypeAPI';
+import { getAllDepartments } from '@/API/DepartmentAPI';
+import { getAllRoles } from '@/API/RoleAPI';
+import { getAllSections } from '@/API/SectionAPI';
+import { getAllSubjectFields } from '@/API/SubjectFieldAPI';
+import { getAllTeams } from '@/API/TeamAPI';
 import AbsenceForm from '@/components/AbsenceForm';
-import { AbsenceType } from '@/types/types';
+import { AbsenceType, Department, Role, Section, SubjectField, Team } from '@/types/types';
 import { useQuery } from '@tanstack/react-query';
 import { createContext, ReactNode, useContext, useState } from 'react';
 
@@ -8,21 +13,35 @@ interface GlobalContextProps {
   children?: ReactNode;
 }
 
-// TODO: add stuff like section setc here as well
 interface GlobalContextType {
   openAbsenceForm: (date?: string) => void;
   absenceTypes: AbsenceType[];
+  roles: Role[];
+  teams: Team[];
+  sections: Section[];
+  departments: Department[];
+  subjectFields: SubjectField[];
 }
 
 const GlobalContext = createContext<GlobalContextType>({
   openAbsenceForm: () => null,
-  absenceTypes: []
+  absenceTypes: [],
+  roles: [],
+  teams: [],
+  sections: [],
+  departments: [],
+  subjectFields: []
 });
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
 const GlobalContextProvider = ({ children }: GlobalContextProps) => {
   const absenceTypes = useQuery(['absence-types'], async () => (await getAllAbsenceTypes()).data);
+  const roles = useQuery(['roles'], async () => (await getAllRoles()).data);
+  const teams = useQuery(['teams'], async () => (await getAllTeams()).data);
+  const sections = useQuery(['sections'], async () => (await getAllSections()).data);
+  const departments = useQuery(['departments'], async () => (await getAllDepartments()).data);
+  const subjectFields = useQuery(['subjectFields'], async () => (await getAllSubjectFields()).data);
 
   const [showAbsenceForm, setShowAbsenceForm] = useState(false);
   const [date, setDate] = useState<string>();
@@ -32,15 +51,34 @@ const GlobalContextProvider = ({ children }: GlobalContextProps) => {
     setShowAbsenceForm(true);
   };
 
-  if (absenceTypes.isLoading) return <div>Loading...</div>;
+  // TODO: Single return or individual?
+  if (absenceTypes.isLoading) return <div>Laster fraværstyper...</div>;
+  if (roles.isLoading) return <div>Laster roller...</div>;
+  if (teams.isLoading) return <div>Laster team...</div>;
+  if (sections.isLoading) return <div>Laster seksjoner...</div>;
+  if (departments.isLoading) return <div>Laster avdelinger...</div>;
+  if (subjectFields.isLoading) return <div>Laster fagfelt...</div>;
 
-  if (absenceTypes.isError) return <div>Error :/</div>;
+  if (
+    absenceTypes.isError ||
+    roles.isError ||
+    teams.isError ||
+    sections.isError ||
+    departments.isError ||
+    subjectFields.isError
+  )
+    return <div>Error :/</div>;
 
   return (
     <GlobalContext.Provider
       value={{
         openAbsenceForm,
-        absenceTypes: absenceTypes.data
+        absenceTypes: absenceTypes.data,
+        roles: roles.data,
+        teams: teams.data,
+        sections: sections.data,
+        departments: departments.data,
+        subjectFields: subjectFields.data
       }}
     >
       {children}
