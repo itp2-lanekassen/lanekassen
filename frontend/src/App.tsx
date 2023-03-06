58a31a32644e104a3e20af16183764c09656a5e0
Simen Sagholen Førrisdal <simenforris@gmail.com>
2023-03-06 12:03:52 +0100

    use gobal data in first time register form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <AuthenticatedTemplate>
         <UserContextProvider>
           <GlobalContextProvider>
-            Router view here
+            {/* Router view here */}
             <FirstTimeRegisterForm />
           </GlobalContextProvider>
         </UserContextProvider>
