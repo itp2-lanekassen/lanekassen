c1a691bf590f9d2071cf22d842fd67c3605d4a1e
Martin Isaksen <martin.isaksen@live.no>
2023-03-15 12:58:15 +0100

    #100 add isApproved field on absence backend

--- backend/Migrations/20230315115641_isApprovedBoolean.Designer.cs
+++ backend/Migrations/20230315115641_isApprovedBoolean.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Lanekassen.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Lanekassen.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230315115641_isApprovedBoolean")]
+    partial class isApprovedBoolean
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
