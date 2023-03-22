3697c55009be732e2d149c27c06f2c74ed2ddbb5
Simen Sagholen Førrisdal <simenforris@gmail.com>
2023-03-22 12:01:21 +0100

    add optional children

--- frontend/src/components/SubmitButton.tsx
+++ frontend/src/components/SubmitButton.tsx
@@ -1,9 +1,12 @@
+import { ReactNode } from 'react';
+
 interface SubmitButtonProps {
   disabledTitle?: string;
   disabled?: boolean;
-  buttonText: string;
+  buttonText?: string;
   handleClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
+  children?: ReactNode;
 }
 
 /**
@@ -20,12 +23,13 @@ export default function SubmitButton({
   handleClick,
   disabled,
   disabledTitle,
+  children,
   type = 'button'
 }: SubmitButtonProps) {
   return (
     <div>
       <button
-        className={`flex text-white rounded-3xl bg-primary pt-3 pl-3 pr-3 pb-3 hover:text-primary hover:bg-white border-solid border-2 ${
+        className={`flex text-white rounded-full bg-primary p-3 hover:text-primary hover:bg-white border-solid border-2 ${
           disabled ? 'bg-gray-300 border-gray-300 cursor-not-allowed' : 'bg-primary border-primary'
         }`}
         onClick={handleClick}
@@ -33,7 +37,7 @@ export default function SubmitButton({
         title={disabled ? disabledTitle : ''}
         type={type}
       >
-        {buttonText}
+        {children || buttonText}
       </button>
     </div>
   );
