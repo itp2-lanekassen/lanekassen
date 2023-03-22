8b0fd1f53af2c613cc8bd52de2386e47b53d233b
Simen Sagholen Førrisdal <simenforris@gmail.com>
2023-03-22 12:00:37 +0100

    remove wrapper div

--- frontend/src/components/DropdownMultiSelect.tsx
+++ frontend/src/components/DropdownMultiSelect.tsx
@@ -7,7 +7,7 @@ export interface DropdownMultiselectProps {
   listOfOptions: { name: string; id: number }[];
   className?: string;
   isExpands?: boolean;
-  isDisabled: boolean;
+  isDisabled?: boolean;
 }
 
 export default function DropdownMultiSelect({
@@ -16,7 +16,7 @@ export default function DropdownMultiSelect({
   listOfOptions,
   value,
   className,
-  isDisabled
+  isDisabled = false
 }: DropdownMultiselectProps) {
   const options = listOfOptions.map(({ name, id }) => ({ label: name, value: id }));
 
@@ -32,36 +32,32 @@ export default function DropdownMultiSelect({
   };
 
   return (
-    <div className="">
-      <Select
-        className={`text-primary w-80 ${className}`}
-        options={options}
-        isDisabled={isDisabled}
-        menuPlacement="auto" // auto menu on top code
-        minMenuHeight={500} // auto menu on top code
-        placeholder={placeholder}
-        isMulti
-        value={value ? options.filter((option) => value.includes(option.value)) : []}
-        onChange={(selectedOptions) => handleChange(selectedOptions.map((o) => o.value))}
-        theme={(theme) => ({
-          ...theme,
-          borderRadius: 20,
-          colors: {
-            ...theme.colors,
-            primary25: '#F6F0F9',
-            primary: '#590689',
-            danger: '#590689',
-            dangerLight: '#D8BCE6',
-            neutral20: '#590689',
-            neutral30: '#590689',
-            neutral50: '#410464',
-            neutral40: '',
-            neutral60: '#590689',
-            neutral80: '#410464'
-          }
-        })}
-        styles={customStyles}
-      />
-    </div>
+    <Select
+      className={`text-primary rounded w-80 ${className}`}
+      options={options}
+      isDisabled={isDisabled}
+      placeholder={placeholder}
+      isMulti
+      value={value ? options.filter((option) => value.includes(option.value)) : []}
+      onChange={(selectedOptions) => handleChange(selectedOptions.map((o) => o.value))}
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: 20,
+        colors: {
+          ...theme.colors,
+          primary25: '#F6F0F9',
+          primary: '#590689',
+          danger: '#590689',
+          dangerLight: '#D8BCE6',
+          neutral20: '#590689',
+          neutral30: '#590689',
+          neutral50: '#410464',
+          neutral40: '',
+          neutral60: '#590689',
+          neutral80: '#410464'
+        }
+      })}
+      styles={customStyles}
+    />
   );
 }
