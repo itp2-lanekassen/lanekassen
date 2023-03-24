6c3725a6a91bc668019aacd6863fc895b4e003b5
Martin Isaksen <martin.isaksen@live.no>
2023-03-24 21:49:27 +0100

    #158 Add holidays API and types

---
+++ frontend/src/API/HolidaysAPI.tsx
@@ -0,0 +1,12 @@
+import { Holiday } from '@/types/types';
+import axios, { AxiosResponse } from 'axios';
+
+const url = 'https://webapi.no/api/v1/Calendar/';
+
+export function getHolidaysByYear(year: number): Promise<AxiosResponse<Holiday[]>> {
+  return axios.get(`${url}${year}`);
+}
+
+export default {
+  getHolidaysByYear
+};
