93f3cad4f7fede3042986cc53c1f64c453ce0549
Hallvard Henriksen <hallvardhen@gmail.com>
2023-03-26 18:42:13 +0200

    add error when delete query fails

--- frontend/src/components/AdminPage/SubjectFieldList.tsx
+++ frontend/src/components/AdminPage/SubjectFieldList.tsx
@@ -2,10 +2,11 @@ import { deleteSubjectField, getAllSubjectFields } from '@/API/SubjectFieldAPI';
 import { SubjectField } from '@/types/types';
 import { Add } from '@mui/icons-material';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import SubmitButton from '../SubmitButton';
 import DeleteButton from './DeleteButton';
 import EditButton from './EditButton';
+import ErrorAlert from '../Alert';
 
 interface SubjectFieldListProps {
   setEdit: (val: boolean, subjectField?: SubjectField) => void;
@@ -13,6 +14,8 @@ interface SubjectFieldListProps {
 
 const SubjectFieldList = ({ setEdit }: SubjectFieldListProps) => {
   const queryClient = useQueryClient();
+  const [errorAlertOpen, setErrorAlertOpen] = useState(false);
+  const [errorAlertMessage, setErrorAlertMessage] = useState('');
 
   const {
     isLoading,
@@ -22,34 +25,41 @@ const SubjectFieldList = ({ setEdit }: SubjectFieldListProps) => {
 
   const { mutate: deleteExistingSubjectField } = useMutation({
     mutationFn: deleteSubjectField,
-    onSuccess: () => queryClient.invalidateQueries(['subjectField'])
+    onSuccess: () => queryClient.invalidateQueries(['subjectField']),
+    onError: (error: Error) => {
+      setErrorAlertMessage('Et fagfelt kan ikke være i bruk før den slettes!');
+      setErrorAlertOpen(true);
+    }
   });
 
   if (isLoading) return <div>Laster...</div>;
   if (isError) return <div>Noe gikk galt</div>;
 
   return (
-    <div className="grid grid-cols-sections text-center gap-x-2 gap-y-3 place-items-center">
-      <div className="heading-3xs">Fagfelt</div>
-      <div className="heading-3xs">Avdeling</div>
-      <div className="col-span-2">
-        <SubmitButton handleClick={() => setEdit(true)}>
-          <Add />
-        </SubmitButton>
+    <>
+      {errorAlertOpen && <ErrorAlert message={errorAlertMessage} />}
+      <div className="grid grid-cols-sections text-center gap-x-2 gap-y-3 place-items-center">
+        <div className="heading-3xs">Fagfelt</div>
+        <div className="heading-3xs">Avdeling</div>
+        <div className="col-span-2">
+          <SubmitButton handleClick={() => setEdit(true)}>
+            <Add />
+          </SubmitButton>
+        </div>
+
+        <div className="col-span-4 border-b-2 w-full" />
+
+        {subjectFields.map((subjectField) => (
+          <Fragment key={subjectField.subjectFieldId}>
+            <div>{subjectField.name}</div>
+            <div>{subjectField.department?.name}</div>
+
+            <EditButton onClick={() => setEdit(true, subjectField)} />
+            <DeleteButton onClick={() => deleteExistingSubjectField(subjectField.subjectFieldId)} />
+          </Fragment>
+        ))}
       </div>
-
-      <div className="col-span-4 border-b-2 w-full" />
-
-      {subjectFields.map((subjectField) => (
-        <Fragment key={subjectField.subjectFieldId}>
-          <div>{subjectField.name}</div>
-          <div>{subjectField.department?.name}</div>
-
-          <EditButton onClick={() => setEdit(true, subjectField)} />
-          <DeleteButton onClick={() => deleteExistingSubjectField(subjectField.subjectFieldId)} />
-        </Fragment>
-      ))}
-    </div>
+    </>
   );
 };
 export default SubjectFieldList;
