62cd8e40b8682820dc90a680a347b0ac68807b96
hallviger <69513598+hallviger@users.noreply.github.com>
2023-03-26 21:19:24 +0200

    add error when delete query fails (#168)

--- frontend/src/components/AdminPage/DepartmentList.tsx
+++ frontend/src/components/AdminPage/DepartmentList.tsx
@@ -2,10 +2,11 @@ import { deleteDepartment, getAllDepartments } from '@/API/DepartmentAPI';
 import { NewDepartment } from '@/types/types';
 import { Add } from '@mui/icons-material';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import SubmitButton from '../SubmitButton';
 import DeleteButton from './DeleteButton';
 import EditButton from './EditButton';
+import ErrorAlert from '../Alert';
 
 interface DepartmentListProps {
   setEdit: (val: boolean, department?: NewDepartment) => void;
@@ -13,6 +14,8 @@ interface DepartmentListProps {
 
 const DepartmentList = ({ setEdit }: DepartmentListProps) => {
   const queryClient = useQueryClient();
+  const [errorAlertOpen, setErrorAlertOpen] = useState(false);
+  const [errorAlertMessage, setErrorAlertMessage] = useState('');
 
   const {
     isLoading,
@@ -22,33 +25,40 @@ const DepartmentList = ({ setEdit }: DepartmentListProps) => {
 
   const { mutate: deleteExistingDepartment } = useMutation({
     mutationFn: deleteDepartment,
-    onSuccess: () => queryClient.invalidateQueries(['departments'])
+    onSuccess: () => queryClient.invalidateQueries(['departments']),
+    onError: (error: Error) => {
+      setErrorAlertMessage('En avdeling kan ikke være i bruk før den slettes!');
+      setErrorAlertOpen(true);
+    }
   });
 
   if (isLoading) return <div>Laster...</div>;
   if (isError) return <div>Noe gikk galt</div>;
 
   return (
-    <div className="grid grid-cols-sections text-center gap-x-2 gap-y-3 place-items-center">
-      <div className="heading-3xs">Avdelingsnavn</div>
-      <div className="heading-3xs">Forkortelse</div>
-      <div className="col-span-2">
-        <SubmitButton handleClick={() => setEdit(true)}>
-          <Add />
-        </SubmitButton>
+    <>
+      {errorAlertOpen && <ErrorAlert message={errorAlertMessage} />}
+      <div className="grid grid-cols-sections text-center gap-x-2 gap-y-3 place-items-center">
+        <div className="heading-3xs">Avdelingsnavn</div>
+        <div className="heading-3xs">Forkortelse</div>
+        <div className="col-span-2">
+          <SubmitButton handleClick={() => setEdit(true)}>
+            <Add />
+          </SubmitButton>
+        </div>
+
+        <div className="col-span-4 border-b-2 w-full" />
+
+        {departments.map((department) => (
+          <Fragment key={department.departmentId}>
+            <div>{department.name}</div>
+            <div>({department.abbreviation})</div>
+            <EditButton onClick={() => setEdit(true, department)} />
+            <DeleteButton onClick={() => deleteExistingDepartment(department.departmentId)} />
+          </Fragment>
+        ))}
       </div>
-
-      <div className="col-span-4 border-b-2 w-full" />
-
-      {departments.map((department) => (
-        <Fragment key={department.departmentId}>
-          <div>{department.name}</div>
-          <div>({department.abbreviation})</div>
-          <EditButton onClick={() => setEdit(true, department)} />
-          <DeleteButton onClick={() => deleteExistingDepartment(department.departmentId)} />
-        </Fragment>
-      ))}
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/AdminPage/RoleList.tsx
+++ frontend/src/components/AdminPage/RoleList.tsx
@@ -2,10 +2,11 @@ import { deleteRole, getAllRoles } from '@/API/RoleAPI';
 import { Role } from '@/types/types';
 import { Add } from '@mui/icons-material';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import SubmitButton from '../SubmitButton';
 import DeleteButton from './DeleteButton';
 import EditButton from './EditButton';
+import ErrorAlert from '../Alert';
 
 interface RoleListProps {
   setEdit: (val: boolean, role?: Role) => void;
@@ -13,6 +14,8 @@ interface RoleListProps {
 
 const RoleList = ({ setEdit }: RoleListProps) => {
   const queryClient = useQueryClient();
+  const [errorAlertOpen, setErrorAlertOpen] = useState(false);
+  const [errorAlertMessage, setErrorAlertMessage] = useState('');
 
   const {
     isLoading,
@@ -22,33 +25,40 @@ const RoleList = ({ setEdit }: RoleListProps) => {
 
   const { mutate: deleteExistingRole } = useMutation({
     mutationFn: deleteRole,
-    onSuccess: () => queryClient.invalidateQueries(['roles'])
+    onSuccess: () => queryClient.invalidateQueries(['roles']),
+    onError: (error: Error) => {
+      setErrorAlertMessage('En rolle kan ikke være i bruk før den slettes!');
+      setErrorAlertOpen(true);
+    }
   });
 
   if (isLoading) return <div>Laster...</div>;
   if (isError) return <div>Noe gikk galt</div>;
 
   return (
-    <div className="grid grid-cols-sections text-center gap-x-2 gap-y-3 place-items-center">
-      <div className="heading-3xs">Rolle</div>
-      <div className="heading-3xs">Avdelinger</div>
-      <div className="col-span-2">
-        <SubmitButton handleClick={() => setEdit(true)}>
-          <Add />
-        </SubmitButton>
+    <>
+      {errorAlertOpen && <ErrorAlert message={errorAlertMessage} />}
+      <div className="grid grid-cols-sections text-center gap-x-2 gap-y-3 place-items-center">
+        <div className="heading-3xs">Rolle</div>
+        <div className="heading-3xs">Avdelinger</div>
+        <div className="col-span-2">
+          <SubmitButton handleClick={() => setEdit(true)}>
+            <Add />
+          </SubmitButton>
+        </div>
+
+        <div className="col-span-4 border-b-2 w-full" />
+
+        {roles.map((role) => (
+          <Fragment key={role.roleId}>
+            <div>{role.name}</div>
+            <div>{role.departments?.map((dep) => dep.name).join(', ')}</div>
+            <EditButton onClick={() => setEdit(true, role)} />
+            <DeleteButton onClick={() => deleteExistingRole(role.roleId)} />
+          </Fragment>
+        ))}
       </div>
-
-      <div className="col-span-4 border-b-2 w-full" />
-
-      {roles.map((role) => (
-        <Fragment key={role.roleId}>
-          <div>{role.name}</div>
-          <div>{role.departments?.map((dep) => dep.name).join(', ')}</div>
-          <EditButton onClick={() => setEdit(true, role)} />
-          <DeleteButton onClick={() => deleteExistingRole(role.roleId)} />
-        </Fragment>
-      ))}
-    </div>
+    </>
   );
 };
 export default RoleList;
