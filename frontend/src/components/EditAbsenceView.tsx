3a349d97ea5d48c996725072e3c01cafff187a67
natalisf <natalisf@stud.ntnu.no>
2023-04-02 20:53:33 +0200

    make EditAbsenceView line up with responsive features of AddAbsenceView

--- frontend/src/components/EditAbsenceView.tsx
+++ frontend/src/components/EditAbsenceView.tsx
@@ -128,11 +128,11 @@ export const EditAbsenceView = (props: { setAbsence: any; absence: Absence }) =>
   };
 
   return (
-    <div className="md:h-[500px] tablet:h-[320px] h-[490px] tablet:w-[400px] relative m-auto">
+    <div className="md:h-[500px] w-full px-[50px] md:px-0 relative m-auto">
       <h3 className="md:ml-[25px] md:text-left text-center md:text-2xl text-xl">Rediger fravær</h3>
       <div className="md:h-[460px] overflow-scroll overflow-x-hidden scrollbar-thin scrollbar-thumb-primary scrollbar-track-primary-lighter hover:scrollbar-thumb-primary-dark scrollbar-thumb-rounded scrollbar-track-rounded">
-        <form className="modal-form" onSubmit={handleSubmit}>
-          <div className="m-auto items-center flex flex-col tablet:flex-row tablet:gap-[20px] tablet:justify-evenly md:w-[350px]">
+        <form className="modal-form md:mx-6" onSubmit={handleSubmit}>
+          <div className="m-auto flex flex-col md:flex-row md:gap-[20px] md:justify-evenly">
             <DateField
               handleInputChange={handleInputChange}
               name="startDate"
@@ -150,22 +150,18 @@ export const EditAbsenceView = (props: { setAbsence: any; absence: Absence }) =>
               label="Til"
             ></DateField>
           </div>
-          <div className="m-auto flex flex-col tablet:flex-row md:flex-col tablet:gap-[20px] tablet:justify-evenly mt-[10px] md:w-[300px]">
-            <div className="w-[190px] md:w-full m-auto">
-              <AbsenceRadioField
-                formValues={formValues}
-                handleRadioChange={handleRadioChange}
-              ></AbsenceRadioField>
-            </div>
-            <div className="w-[190px] md:w-full m-auto">
-              <CommentField
-                handleInputChange={handleTextAreaChange}
-                placeholder={props.absence.comment}
-                formValues={formValues}
-              ></CommentField>
-            </div>
+          <div className="m-auto flex flex-col md:flex-row md:flex-col md:gap-[20px] md:justify-evenly mt-[10px] md:w-[350px]">
+            <AbsenceRadioField
+              formValues={formValues}
+              handleRadioChange={handleRadioChange}
+            ></AbsenceRadioField>
+            <CommentField
+              handleInputChange={handleTextAreaChange}
+              placeholder={props.absence.comment}
+              formValues={formValues}
+            ></CommentField>
           </div>
-          <div className="m-auto w-[300px] flex justify-center gap-[20px]">
+          <div className="m-auto flex justify-center gap-[20px]">
             <SubmitButton
               disabledTitle={'Fyll ut alle feltene'}
               disabled={false}
