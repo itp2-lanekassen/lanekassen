aeb72e8f6eb585b257ecbc645ecc8e7405ee2bc2
natalisf <natalisf@stud.ntnu.no>
2023-04-12 20:11:15 +0200

    let header scale width screen width

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -10,8 +10,8 @@ const PageLayout = ({ title, children }: PageLayoutProps) => {
   return (
     <main className="min-h-screen w-full max-w-screen-xl mx-auto pb-6">
       <div className="flex justify-center relative h-1/4-screen">
-        <img className="w-9/12 h-1/5-screen" src={ellipse} alt="" />
-        <h1 className="absolute top-1/3 -translate-y-1/3 md:text-3xl text-2xl">{title}</h1>
+        <img className="w-[85vw] h-[16vw] md:h-[10vw]" src={ellipse} alt="" />
+        <h1 className="absolute top-[4vw] -translate-y-1/3 sm:text-3xl text-2xl">{title}</h1>
       </div>
       {children}
     </main>
