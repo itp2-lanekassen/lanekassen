2b41dc0cf06a62766e2883da024190a5be50ebfc
natalisf <natalisf@stud.ntnu.no>
2023-04-15 18:44:32 +0200

    close side menu on phone when click outside

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -10,6 +10,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useMsal } from '@azure/msal-react';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 interface PageLayoutProps {
   title: string;
@@ -42,52 +43,60 @@ const PageLayout = ({ title, children }: PageLayoutProps) => {
       >
         <MenuIcon className="text-white" />
       </button>
-      <div
-        style={{ left: sideMenuVisibility }}
-        className={`bg-primary flex flex-col items-center gap-[2vh] pt-[10vw] h-full text-xs sm:hidden fixed top-0 w-[40vw] duration-300 z-10 overflow-hidden`}
+      <OutsideClickHandler
+        onOutsideClick={() => {
+          setSideMenuVisibility('-40vw');
+          console.log('hei');
+        }}
       >
-        <button
-          onClick={() => {
-            instance.logoutRedirect({
-              postLogoutRedirectUri: '/'
-            });
-          }}
-          className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
+        <div
+          style={{ left: sideMenuVisibility }}
+          className={`bg-primary flex flex-col items-center gap-[2vh] pt-[10vw] h-full text-xs sm:hidden fixed top-0 w-[40vw] duration-300 z-10 overflow-hidden`}
         >
-          <LogoutIcon className="text-white ml-[4vw] mr-[2vw]"></LogoutIcon>
-          Logg ut
-        </button>
-        <button
-          onClick={() => navigate('/')}
-          className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
-        >
-          <CalendarMonthIcon className="text-white ml-[4vw] mr-[2vw]"></CalendarMonthIcon>
-          Kalender
-        </button>
-        <button
-          onClick={() => navigate('/profil')}
-          className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
-        >
-          <PersonIcon className="text-white ml-[4vw] mr-[2vw]"></PersonIcon>
-          Profil
-        </button>
-        <button
-          onClick={() => navigate('/fravaersside')}
-          className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
-        >
-          <EditCalendarIcon className="text-white ml-[4vw] mr-[2vw]"></EditCalendarIcon>
-          Dine fravær
-        </button>
-        {currentUser.admin && (
           <button
-            onClick={() => navigate('/admin')}
-            className="w-[90%] h-[40px] rounded-[5px] bg-primary-dark hover:brightness-125 text-white text-left"
+            onClick={() => {
+              instance.logoutRedirect({
+                postLogoutRedirectUri: '/'
+              });
+            }}
+            className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
           >
-            <AdminPanelSettingsIcon className="text-white ml-[4vw] mr-[2vw]"></AdminPanelSettingsIcon>
-            Admin
+            <LogoutIcon className="text-white ml-[4vw] mr-[2vw]"></LogoutIcon>
+            Logg ut
           </button>
-        )}
-      </div>
+          <button
+            onClick={() => navigate('/')}
+            className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
+          >
+            <CalendarMonthIcon className="text-white ml-[4vw] mr-[2vw]"></CalendarMonthIcon>
+            Kalender
+          </button>
+          <button
+            onClick={() => navigate('/profil')}
+            className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
+          >
+            <PersonIcon className="text-white ml-[4vw] mr-[2vw]"></PersonIcon>
+            Profil
+          </button>
+          <button
+            onClick={() => navigate('/fravaersside')}
+            className="w-[90%] rounded-[5px] h-[40px] hover:brightness-125 bg-primary-dark text-white text-left"
+          >
+            <EditCalendarIcon className="text-white ml-[4vw] mr-[2vw]"></EditCalendarIcon>
+            Dine fravær
+          </button>
+          {currentUser.admin && (
+            <button
+              onClick={() => navigate('/admin')}
+              className="w-[90%] h-[40px] rounded-[5px] bg-primary-dark hover:brightness-125 text-white text-left"
+            >
+              <AdminPanelSettingsIcon className="text-white ml-[4vw] mr-[2vw]"></AdminPanelSettingsIcon>
+              Admin
+            </button>
+          )}
+        </div>
+      </OutsideClickHandler>
+
       <div className="absolute flex-col gap-[5px] hidden sm:flex items-end place-items-right w-[200px] top-[5px] right-[5px]">
         <SignOutButton />
         <button
