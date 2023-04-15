84ca785f981889705055bd0a193892c195f4a1cd
Synne Ødegaard <synne.odegaard@gmail.com>
2023-04-15 19:05:20 +0200

    Remove console log

--- frontend/src/components/calendar/UserDropdown.tsx
+++ frontend/src/components/calendar/UserDropdown.tsx
@@ -45,7 +45,6 @@ export default function UserDropdown(props: { user: User; isCurrentUser: boolean
 
     props.user.teams?.forEach((team) => listTeam.push(team.name));
     setTeams(listTeam);
-    console.log(props.user);
 
     props.user.subjectFields?.forEach((subjectField) => listSubjectField.push(subjectField.name));
     setSubjectFields(listSubjectField);
