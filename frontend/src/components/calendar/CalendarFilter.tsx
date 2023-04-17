e2fc0baa95cb47c9ccec8992abc1be5074237f5b
Simon Nymo <simon.nymo@gmail.com>
2023-04-17 13:09:58 +0200

    make descriptionbox beautiful

--- frontend/src/components/calendar/CalendarFilter.tsx
+++ frontend/src/components/calendar/CalendarFilter.tsx
@@ -204,21 +204,21 @@ export default function FilterComponents() {
           </button>
           {showDescription && (
             <div
-              className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-primary z-50 px-4 py-2 rounded-lg text-primary-contrast"
+              className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-card-one z-50 px-4 py-2 rounded-lg text-primary border-primary border-2"
               onMouseEnter={() => setShowDescription(true)}
               onMouseLeave={() => setShowDescription(false)}
             >
-              <h2 className="text-xl mb-6 text-primary-contrast">Forklaring av farger og koder</h2>
+              <h2 className="text-xl mb-6 text-primary">Forklaring av farger og koder</h2>
               <div className="flex flex-col">
                 {absenceTypes.map((type) => (
                   <div
                     key={type.absenceTypeId}
                     className="w-100 inline-flex justify-start items-center md:heading-2xs text-xs"
                   >
-                    <div className="mb-2">
+                    <div className="mb-2 pr-2">
                       <SingleCalendarCellDisplay code={type.code} colorCode={type.colorCode} />
                     </div>
-                    <span className="overflow-hidden text-ellipsis text-primary-contrast whitespace-nowrap">
+                    <span className="overflow-hidden text-ellipsis text-primary whitespace-nowrap pb-1">
                       {type.name}
                     </span>
                   </div>
