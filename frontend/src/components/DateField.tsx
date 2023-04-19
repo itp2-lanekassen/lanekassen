6710d86482bb5fa98f1bceffbc341eb98635731e
Hallvard Henriksen <hallvardhen@gmail.com>
2023-04-19 11:26:32 +0200

    a bit wonky keyboard handling but works

--- frontend/src/components/DateField.tsx
+++ frontend/src/components/DateField.tsx
@@ -1,3 +1,5 @@
+import { addDays, addWeeks, startOfMonth } from 'date-fns';
+import { useEffect, useRef, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -39,6 +41,49 @@ export const DateField = (props: {
   if (props.min) {
     min = props.min;
   }
+  const datePickerRef = useRef<DatePicker>(null);
+
+  const [tempDate, setTempDate] = useState<Date | null>(props.value || null);
+
+  const onMonthChange = (date: Date) => {
+    const firstDayOfMonth = startOfMonth(date);
+    props.handleInputChange(firstDayOfMonth, undefined, props.name);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log('handleKeyDown', tempDate, e.key);
+    switch (e.key) {
+      case 'ArrowLeft':
+        setTempDate(addDays(tempDate || props.value!, -1));
+        datePickerRef.current?.setSelected(addDays(tempDate || props.value!, -1));
+        break;
+
+      case 'ArrowRight':
+        setTempDate(addDays(tempDate || props.value!, +1));
+        datePickerRef.current?.setSelected(addDays(tempDate || props.value!, +1));
+        break;
+
+      case 'ArrowDown':
+        setTempDate(addWeeks(tempDate || props.value!, +1));
+        datePickerRef.current?.setSelected(addWeeks(tempDate || props.value!, +1));
+        break;
+
+      case 'ArrowUp':
+        setTempDate(addWeeks(tempDate || props.value!, -1));
+        datePickerRef.current?.setSelected(addWeeks(tempDate || props.value!, -1));
+        break;
+
+      case 'Enter':
+        props.handleInputChange(tempDate, e, props.name);
+        e.preventDefault();
+        e.stopPropagation();
+        break;
+    }
+  };
+
+  useEffect(() => {
+    setTempDate(props.value || null);
+  }, [props.value]);
 
   return (
     <div className="modal-field">
@@ -46,7 +91,7 @@ export const DateField = (props: {
         {props.label}
       </label>
       <DatePicker
-        selected={props.value}
+        selected={tempDate}
         autoComplete="off"
         id="datePicker"
         excludeDates={props.disableArray}
@@ -54,16 +99,20 @@ export const DateField = (props: {
         minDate={min}
         maxDate={max}
         calendarStartDay={1}
-        /*value={props.value?.toLocaleDateString()} */
         onChange={handleInputChange}
         showWeekNumbers
-        dateFormat="MM/dd/yyyy"
+        dateFormat="dd/MM/yyyy"
         className={`modal-input heading-2xs py-3 w-full border-2 rounded-[20px] border-primary text-center bg-primary-contrast disabled:bg-primary-contrast-lighter disabled:cursor-not-allowed disabled:opacity-50 disabled:text-primary-contrast-lighter ${
           props.customClass ? props.customClass : ''
         }`}
         required
         disabled={props.disabled}
         title={props.disabled === false ? props.title : props.title}
+        onKeyDown={handleKeyDown}
+        weekLabel="Uke"
+        startOpen={false}
+        ref={datePickerRef}
+        onMonthChange={onMonthChange}
       />
     </div>
   );
