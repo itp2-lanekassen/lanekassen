403269a5fa0835f6e10356822d8c0504150cc666
Martin Isaksen <martin.isaksen@live.no>
2023-04-19 10:47:33 +0200

    #175 Fix height on mobile

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -73,7 +73,7 @@ const PageLayout = ({ title, children }: PageLayoutProps) => {
 
   return (
     <main className="min-h-screen w-full max-w-screen-xl mx-auto pb-6">
-      <div className="flex justify-center relative h-1/4-screen">
+      <div className="flex justify-center relative h-1/8 md:h-1/4-screen">
         <img className="w-[85vw] h-[16vw] md:h-[10vw]" src={ellipse} alt="" />
         <h1 className="absolute top-[4vw] -translate-y-1/3 sm:text-3xl text-2xl">{title}</h1>
       </div>
