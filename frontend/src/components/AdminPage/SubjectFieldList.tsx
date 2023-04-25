e8db75ecc34510a0fe91bc01b5414b0b9a99b964
Synne Ødegaard <synne.odegaard@gmail.com>
2023-04-25 21:31:10 +0200

    Align SubjectField text to the left

--- frontend/src/components/AdminPage/SubjectFieldList.tsx
+++ frontend/src/components/AdminPage/SubjectFieldList.tsx
@@ -48,9 +48,9 @@ const SubjectFieldList = ({ setEdit }: SubjectFieldListProps) => {
   return (
     <>
       {errorAlertOpen && <ErrorAlert message={errorAlertMessage} />}
-      <div className="grid grid-cols-sections gap-x-2 gap-y-3 place-items-center">
-        <div className="heading-3xs">Fagområde</div>
-        <div className="heading-3xs">Avdeling</div>
+      <div className="grid grid-cols-sections gap-x-2 gap-y-3 items-center">
+        <div className="heading-3xs md:ml-20">Fagområde</div>
+        <div className="heading-3xs text-center">Avdeling</div>
         <div className="col-span-2">
           <SubmitButton handleClick={() => setEdit(true)}>
             <Add />
@@ -61,8 +61,10 @@ const SubjectFieldList = ({ setEdit }: SubjectFieldListProps) => {
 
         {subjectFields.map((subjectField) => (
           <Fragment key={subjectField.subjectFieldId}>
-            <div>{subjectField.name}</div>
-            <div>{subjectField.department?.name}</div>
+            <div className="md:ml-20">{subjectField.name}</div>
+            <div className="ml-[35%] xl:ml-44 lg:ml-[40%] md:ml-[40%]">
+              {subjectField.department?.name}
+            </div>
 
             <EditButton onClick={() => setEdit(true, subjectField)} />
             <DeleteButton
