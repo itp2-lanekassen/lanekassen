773fe0da8c401943a6175fc7a9c11aea2699b4d4
Synne Ødegaard <synne.odegaard@gmail.com>
2023-04-27 22:18:17 +0200

    Write E2E tests for URL navigation

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    testIsolation: false
+    }
   }
 });
--- frontend/cypress/e2e/urls.cy.ts
+++ frontend/cypress/e2e/urls.cy.ts
@@ -1,11 +1,31 @@
 describe('Login', () => {
   it('Logs in', () => {
-    cy.visit('http://localhost:3000/profil');
-    cy.get('button').contains('Logg inn med Microsoft Azure').click();
+    cy.visit('http://localhost:3000');
+    cy.get('button').click();
+    cy.get('h1').contains('Kalender');
   });
 });
+
 describe('Test profile URL', () => {
-  it('Check that profile page is displayed', () => {
+  it('Navigate to profile', () => {
+    cy.visit('http://localhost:3000/profil');
+    cy.get('button').click();
     cy.get('h1').contains('Profil');
+  });
+});
+
+describe('Test absence page URL', () => {
+  it('Navigate to absence page', () => {
+    cy.visit('http://localhost:3000/fravaersside');
+    cy.get('button').click();
+    cy.get('h1').contains('Mine fravær');
+  });
+});
+
+describe('Test admin page URL', () => {
+  it('Navigate to admin page', () => {
+    cy.visit('http://localhost:3000/admin');
+    cy.get('button').click();
+    cy.get('h1').contains('Admin');
   });
 });
