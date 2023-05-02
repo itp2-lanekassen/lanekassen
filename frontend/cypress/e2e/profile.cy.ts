93f888e8c45fd22ca37e0232ece8c26bd1c2bdd2
Synne Ødegaard <synne.odegaard@gmail.com>
2023-05-02 21:24:01 +0200

    Try new test

--- frontend/cypress/e2e/profile.cy.ts
+++ frontend/cypress/e2e/profile.cy.ts
@@ -7,10 +7,16 @@ describe('Navigate to profile from calendar', () => {
   it('Navigate and validate that the page is correct', () => {
     cy.visit('http://localhost:3000/');
     cy.get('button').contains('Logg inn med Microsoft Azure').click();
-    //cy.contains('Profil').invoke('show').click();
     cy.get('h1').contains('Kalender');
     cy.get('button').contains('Profil').click({ force: true });
-    //cy.get('span').contains('Profil').trigger('mouseover').wait(1000).should('be.visible').click();
     cy.get('h1').contains('Profil');
+  });
+});
+
+describe('Edit user', () => {
+  it('Check that edit button works', () => {
+    cy.visit('http://localhost:3000/profil');
+    cy.get('button').contains('Logg inn med Microsoft Azure').click();
+    cy.get('button').contains('Rediger').click();
   });
 });
