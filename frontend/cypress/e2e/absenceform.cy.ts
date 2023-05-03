bc7771cd604814ac91dbad75cd529be9de6a1157
Synne Ødegaard <synne.odegaard@gmail.com>
2023-05-03 10:07:05 +0200

    Add more tests for absence form

--- frontend/cypress/e2e/absenceform.cy.ts
+++ frontend/cypress/e2e/absenceform.cy.ts
@@ -9,15 +9,17 @@ describe('Navigate to profile from calendar', () => {
 });
 
 describe('Test absence form', () => {
-  /*
-    it('Add absence', () => {
-        cy.visit('http://localhost:3000/fravaersside');
-        cy.get('button').contains('Logg inn med Microsoft Azure').click();
-        cy.get('#startDate').type('10.10.2023').wait(1000);
-        cy.get('#endDate').type('16.10.2023');
-        cy.get('#comment').type('Going on vacation');
-        cy.contains('Lagre').click();
-    });*/
+  it('Add absence', () => {
+    cy.visit('http://localhost:3000/fravaersside');
+    cy.get('button').contains('Logg inn med Microsoft Azure').click();
+    cy.get('#startDate').type('10.10.2023');
+    cy.get('#endDate').type('16.10.2023');
+    cy.get('#comment').type('Going on vacation');
+    cy.contains('Lagre').click();
+    cy.contains('10.10.2023 - 16.10.2023').click();
+    cy.contains('Fraværstype: Tilgjengelig');
+    cy.contains('Going on vacation');
+  });
   it('Edit absence', () => {
     cy.visit('http://localhost:3000/fravaersside');
     cy.get('button').contains('Logg inn med Microsoft Azure').click();
@@ -29,12 +31,16 @@ describe('Test absence form', () => {
     cy.contains('Lagre').click();
     cy.contains('10.10.2023 - 16.10.2023').should('not.exist');
     cy.contains('11.10.2023 - 17.10.2023');
+    cy.contains('11.10.2023 - 17.10.2023').click();
+    cy.contains('Is this edited?');
   });
   it('Delete absence', () => {
     cy.visit('http://localhost:3000/fravaersside');
     cy.get('button').contains('Logg inn med Microsoft Azure').click();
-    cy.contains('10.10.2023 - 16.10.2023').click();
+    cy.contains('11.10.2023 - 17.10.2023').click();
     cy.get('svg[data-testid="DeleteOutlineIcon"]').click();
     cy.contains('Ja').click();
+    cy.contains('10.10.2023 - 16.10.2023').should('not.exist');
+    cy.contains('11.10.2023 - 17.10.2023').should('not.exist');
   });
 });
